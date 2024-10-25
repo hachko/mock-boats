df403e1ac6d5657f76aa614a479b239e208d76dd
jaknichan <jaknichan@yahoo.fr>
2024-10-25 15:00:29 +0200

    #7 add Roles in admin users menu

--- front/src/app/user-admin/user-admin.component.ts
+++ front/src/app/user-admin/user-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../users.service';
-import { User } from '../model/user.model';
+import { User, Role } from '../model/user.model';
+import { RoleName } from '../model/role-name-enum.model';
 
 @Component({
   selector: 'app-user-admin',
@@ -9,6 +10,8 @@ import { User } from '../model/user.model';
 })
 export class UserAdminComponent {
   users: User[] = [];
+  roleNames = Object.values(RoleName);
+  selectedRole?: RoleName;
   newUser: User = { id: null, username: '', password: '', roles: [] };
 
   constructor(private userService: UserService) {}
@@ -24,7 +27,7 @@ export class UserAdminComponent {
   addUser(): void {
     if (!this.newUser.username.trim() || !this.newUser.password.trim()) {
       return;
-    }
+    }    
     this.userService.addUser(this.newUser).subscribe(user => {
       this.users.push(user);
       this.newUser = { id: 0, username: '', password: '', roles: [] };
@@ -37,6 +40,19 @@ export class UserAdminComponent {
         this.users = this.users.filter(user => user!== user);
       });
     }    
+  }
+
+  onRoleChange(event: any): void {
+    const role: RoleName = event.target.value;
+    if (event.target.checked) {
+      this.newUser.roles.push({name: role});
+    } else {
+      this.newUser.roles = this.newUser.roles.filter(r => r.name !== role);
+    }
+  }
+
+  isRoleSelected(roleName: RoleName): boolean {
+    return this.newUser.roles.some(r => r.name === roleName);
   }
 
   getUserRolesInCommaSeparatedString(user: User): string {
